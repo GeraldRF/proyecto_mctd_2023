7ff3d9c578486d28e34ec76d1b06b0672a275e0e
Gerald Ramirez <geral.rafo@gmail.com>
2023-06-04 09:30:08 -0600

    layout from grid to pack

--- src/ui/program_ui.py
+++ src/ui/program_ui.py
@@ -10,29 +10,30 @@ class ProgramUI:
         self.root = tk.Tk()
         
         self.mainFrame = ttk.Frame(self.root, padding=10)
-        self.mainFrame.grid()        
-        
-        self.run_btn = ttk.Button(self.mainFrame)
+        self.mainFrame.pack()        
         
         self.selectFileFrame = ttk.Frame(self.mainFrame)
         self.file_label = ttk.Label(self.selectFileFrame)
         
+        self.run_btn = ttk.Button(self.mainFrame)
+        
         self.init_components()
         
     def init_components(self):
-        ttk.Label(self.mainFrame, text="Hello user, please select a csv file.").grid(column=0, row=0, pady=10)
+        ttk.Label(self.mainFrame, text="Hello user, please select a csv file.").pack(pady=10)
         
-        self.run_btn.grid(column=0, row=2)
-        self.run_btn.config(text="Run program")
         
-        ttk.Button(self.mainFrame, text="Close", command=self.root.destroy).grid(column=1, row=2)
+        self.selectFileFrame.pack(pady=30, padx=20)
         
-        self.selectFileFrame.grid(column=0, row=1)
-        
-        ttk.Button(self.selectFileFrame, text="Select", command=self.open_file_dialog).grid(column=0, row=0)
-        
-        self.file_label.grid(column=1, row=0)
+        ttk.Button(self.selectFileFrame, text="Select", command=self.open_file_dialog, width=30).pack(padx=20)
+        self.run_btn.pack()
         self.file_label.config(text="You don't selected any file")
+        self.file_label.pack()
+        
+        self.run_btn.config(text="Run program", width=30)
+        
+        ttk.Button(self.mainFrame, text="Close", width=30, command=self.root.destroy).pack()
+        
 
     def open_file_dialog(self):
         self.file_path = filedialog.askopenfilename()
