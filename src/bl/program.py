53b537b7cb18142d84dfd70219272bec8cfb3d04
DilbertRV <gearxdtrece@gmail.com>
2023-06-06 20:43:17 -0600

    Changes in files

--- src/bl/program.py
+++ src/bl/program.py
@@ -1,19 +1,38 @@
-import pandas as pd
 import math
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from statsmodels.tsa.api import Holt
+
 
 class Program: 
     def __init__(self, window):
         self.window = window
-        window.run_btn.config(command=self.run_program)
-        window.run()
-        
-    def run_program(self):
-        if(self.window.file_path):
-            file = pd.read_csv(self.window.file_path)
-            count = 0
-            for line in file["TAVG"]:
-                if math.isnan(line):
-                    count+=1
-            print(count)
-        else: 
-            self.window.file_label.config(foreground="red", text="Please, select a file first!")
+        # print(self.window.frame.button.cget("text"))
+        self.window.frame.button.configure(command=self.holt)
+        self.window.mainloop()
+
+    def holt(self):
+        datos = self.window.file["TAVG"]
+        entrenamiento_porcentaje = 0.8
+        num_entrenamiento = int(len(datos) * entrenamiento_porcentaje)
+        entrenamiento = datos[:num_entrenamiento]
+        prueba = datos[num_entrenamiento:]
+        modelo = Holt(entrenamiento)
+        ajuste = modelo.fit()
+        proyeccion = ajuste.forecast(steps=len(prueba))
+        plt.plot(range(num_entrenamiento), entrenamiento, label='Entrenamiento')
+        plt.plot(range(num_entrenamiento, len(datos)), prueba, label='Prueba')
+        plt.plot(range(num_entrenamiento, len(datos)), proyeccion, label='Proyección')
+        plt.legend()
+        plt.show()
+    
+    def obtainFile(self): 
+        datos = self.window.file["TAVG"]
+        modelo = Holt(datos)
+        ajuste = modelo.fit(optimized=True)
+        proyeccion = ajuste.forecast(steps=80)
+        plt.plot(datos, label='Datos')
+        plt.plot(range(len(datos), len(datos) + len(proyeccion)), proyeccion, label='Proyección')
+        plt.legend()
+        plt.show()
--- src/ui/program_ui.py
+++ src/ui/program_ui.py
@@ -1,48 +1,75 @@
-import tkinter as tk
-from tkinter import ttk
+import customtkinter
 from tkinter import filedialog
+from tkinter import ttk 
+import pandas as pd
 
-class ProgramUI:
-    
+
+class MySecondFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.button = customtkinter.CTkButton(self, text="Realizar Proyección HOLT")
+        self.button.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+        
+
+
+class ProgramUI(customtkinter.CTk):    
     def __init__(self):
-        self.file_path = None
-        
-        self.root = tk.Tk()
-        
-        self.mainFrame = ttk.Frame(self.root, padding=10)
-        self.mainFrame.pack()        
-        
-        self.selectFileFrame = ttk.Frame(self.mainFrame)
-        self.file_label = ttk.Label(self.selectFileFrame)
-        
-        self.options_panel = ttk.Frame(self.mainFrame)
-        
-        self.run_btn = ttk.Button(self.options_panel)
-        
-        self.init_components()
-        
-    def init_components(self):
-        ttk.Label(self.mainFrame, text="Hello user, please select a csv file.").pack(pady=10)
-        
-        
-        self.selectFileFrame.pack(pady=30, padx=20)
-        
-        ttk.Button(self.selectFileFrame, text="Select", command=self.open_file_dialog, width=30).pack(padx=20)
-        self.run_btn.pack()
-        self.file_label.config(text="You don't selected any file")
-        self.file_label.pack()
-        
-        self.options_panel.pack(pady=5, padx=20)
-        self.run_btn.config(text="Run program", width=20)
-        self.run_btn.pack(side="left", padx=5)
-        
-        ttk.Button(self.options_panel, text="Close", width=20, command=self.root.destroy).pack(side="right", padx=5)
-        
+        super().__init__()
 
+        self.file = None
+
+        self.title("Proyecto - Métodos Cuantitativos 2023")
+        self.geometry("800x600")
+        self.grid_columnconfigure(0, weight=1)
+
+        customtkinter.set_appearance_mode("dark")
+
+        label = customtkinter.CTkLabel(self, text="Selecciona un archivo para habilitar las acciones.", fg_color="transparent")
+        label.grid(row=0, column=0, padx=20, pady=5, sticky="ew" )
+
+        self.file_label = customtkinter.CTkLabel(self, text="No se ha seleccionado ningún archivo.", fg_color="transparent")
+        self.file_label.grid(row=1, column=0, padx=20, pady=5, sticky="ew" )
+
+        button = customtkinter.CTkButton(self, text="Seleccionar .csv", command=self.open_file_dialog)
+        button.grid(row=2, column=0, padx=20, pady=15, sticky="ew")
+        
+        #Tabla de datos
+        self.treeview = ttk.Treeview(self)
+        self.treeview.grid(row=3, column=0, padx=20, pady=10, sticky="nsew")
+        self.treeview["columns"] = ("Fecha", "TAVG")  # Agrega las columnas necesarias
+        self.treeview.heading("#0", text="C. Datos")
+        self.treeview.column("#0", width=80, anchor="center", stretch=False, minwidth=80)
+        for col in self.treeview["columns"]:
+            self.treeview.heading(col, text=col)
+            self.treeview.column(col, width=80, anchor="center")
+        #Scroll de la tabla
+        scroll_y = customtkinter.CTkScrollbar(self, command=self.treeview.yview)
+        scroll_y.grid(row=3, column=1, sticky="ns")
+        self.treeview.configure(yscrollcommand=scroll_y.set)    
+
+
+        self.frame = MySecondFrame(self)
+        self.frame.grid(row=4, column=0, padx=20, pady=(10, 0), sticky="nsw")
+
+    #Acciones de la UI        
+    def button_callback(self):
+         print("button pressed")    
     def open_file_dialog(self):
         self.file_path = filedialog.askopenfilename(filetypes=[('CSV', '*.csv' )])
         if(self.file_path):
-            self.file_label.config(text=self.file_path)
+            self.file_label.configure(text=self.file_path)
+            self.show_data()
+
+    def show_data(self):
+        if self.file_path:
+            self.file = pd.read_csv(self.file_path)
+            # Borra las filas existentes en la tabla
+            self.treeview.delete(*self.treeview.get_children())
+            # Inserta los datos
+            for i, row in self.file.iterrows():
+                date = row["DATE"]
+                tavg = f"{row['TAVG']} C°"
+                self.treeview.insert(parent="", index="end", iid=i, text=str(i), values=(date, tavg))  
         
-    def run(self):
-        self.root.mainloop()
+        
